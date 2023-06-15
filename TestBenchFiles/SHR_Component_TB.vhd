9f2f5407c40848decb774c5575441bef76d22269
Matthew Howey <matthew.howey@yahoo.co.uk>
2023-06-15 19:44:35 +0100

    add remainder in to SHR

--- TestBenchFiles/SHR_Component_TB.vhd
+++ TestBenchFiles/SHR_Component_TB.vhd
@@ -13,23 +13,26 @@ architecture Behavioral of SHR_Component_TB is
     component SHR_Component
         port (
             a       : in  std_logic_vector(7 downto 0);
+	    remain_in : in std_logic;
             y       : out std_logic_vector(7 downto 0);
-	    remain  : out std_logic
+	    remain_out  : out std_logic
         );
     end component SHR_Component;
 
     -- Signal declarations
     signal a_tb    : std_logic_vector(7 downto 0);
+    signal remain_in_tb    : std_logic;
     signal y_tb    : std_logic_vector(7 downto 0);
-    signal remain_tb    : std_logic;
+    signal remain_out_tb    : std_logic;
 
 begin
     -- Instantiate the SHR_Component module
     UUT: SHR_Component
         port map (
             a       => a_tb,
+	    remain_in  => remain_in_tb,
             y       => y_tb,
-	    remain  => remain_tb
+	    remain_out => remain_out_tb
         );
 
     -- Stimulus process to apply test vectors
@@ -41,44 +44,49 @@ begin
 
         -- Test case 1
         a_tb <= "00000000";
+	remain_in_tb <= '0';
         wait for 10 ns;
 
 	report "Running Test case 1";
 	assert y_tb = "00000000" report "y_tb should equal 00000000" severity error;
-	assert remain_tb = '0' report "remain_tb should equal 0" severity error;
+	assert remain_out_tb = '0' report "remain_tb should equal 0" severity error;
 
 
         -- Test case 2
         a_tb <= "11111111";
+	remain_in_tb <= '1';
         wait for 10 ns;
 
 	report "Running Test case 2";
-	assert y_tb = "01111111" report "y_tb should equal 01111111" severity error;
-	assert remain_tb = '1' report "remain_tb should equal 1" severity error;
+	assert y_tb = "11111111" report "y_tb should equal 11111111" severity error;
+	assert remain_out_tb = '1' report "remain_tb should equal 1" severity error;
 
         -- Test case 3
         a_tb <= "10101010";
+	remain_in_tb <= '0';
         wait for 10 ns;
 
 	report "Running Test case 3";
 	assert y_tb = "01010101" report "y_tb should equal 01010101" severity error;
-	assert remain_tb = '0' report "remain_tb should equal 0" severity error;
+	assert remain_out_tb = '0' report "remain_tb should equal 0" severity error;
 
 	-- Test case 4
         a_tb <= "01010101";
+	remain_in_tb <= '1';
         wait for 10 ns;
 
 	report "Running Test case 4";
-	assert y_tb = "00101010" report "y_tb should equal 00101010" severity error;
-	assert remain_tb = '1' report "remain_tb should equal 1" severity error;
+	assert y_tb = "10101010" report "y_tb should equal 10101010" severity error;
+	assert remain_out_tb = '1' report "remain_tb should equal 1" severity error;
 
 	-- Test case 5
         a_tb <= "10110101";
+	remain_in_tb <= '0';
         wait for 10 ns;
 
 	report "Running Test case 5";
 	assert y_tb = "01011010" report "y_tb should equal 01011010" severity error;
-	assert remain_tb = '1' report "remain_tb should equal 1" severity error;
+	assert remain_out_tb = '1' report "remain_tb should equal 1" severity error;
 
         -- Add more test cases here if needed
 
