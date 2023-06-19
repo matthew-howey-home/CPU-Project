a3d26caf8cbd2faf1bddf0d1966aed262f0296d8
Matthew Howey <matthew.howey@yahoo.co.uk>
2023-06-19 20:33:27 +0100

    add tests for two tristate buffers together

--- TestBenchFiles/Tristate_Buffer_TB.vhd
+++ TestBenchFiles/Tristate_Buffer_TB.vhd
@@ -18,17 +18,27 @@ architecture Behavioral of Tristate_Buffer_TB is
     end component Tristate_Buffer;
 
     -- Signal declarations
-    signal a_tb   : std_logic_vector(7 downto 0);
-    signal en_tb : std_logic;
-    signal y_tb    : std_logic_vector(7 downto 0);
-   
+    signal a_tb_1   : std_logic_vector(7 downto 0);
+    signal en_tb_1 : std_logic;
+    
+    signal a_tb_2   : std_logic_vector(7 downto 0);
+    signal en_tb_2 : std_logic;
 
+    signal y_tb   : std_logic_vector(7 downto 0);
+   
 begin
     -- Instantiate the Tristate_Buffer module
-    UUT: Tristate_Buffer
+    UUT1: Tristate_Buffer
         port map (
-            a       => a_tb,
-            en       => en_tb,
+            a       => a_tb_1,
+            en       => en_tb_1,
+            y    => y_tb
+        );
+
+    UUT2: Tristate_Buffer
+        port map (
+            a       => a_tb_2,
+            en       => en_tb_2,
             y    => y_tb
         );
 
@@ -38,23 +48,64 @@ begin
     begin
 	report "Running Tests for Tristate Buffer Component";
 
-        -- Test case 1
-        a_tb <= "00101011";
-	-- enable is active low
-        en_tb <= '0';
+	-- Set UUT2 to not enabled so we can test UUT1 in isolation
+	-- note enable is active low, so 1 means not enabled
+	en_tb_2 <= '1';
+
+
+	-- Test case 1 (for UUT1 only)
+        a_tb_1 <= "00101011";
+	-- enable is active low, so 0 means enable
+        en_tb_1 <= '0';
         wait for 10 ns;
 	
 	report "Running Test case 1";
 	assert y_tb = "00101011" report "Test 1: y_tb should equal 00101011" severity error;
 
-	-- Test case 2
-        a_tb <= "00101011";
-	-- enable is active low
-        en_tb <= '1';
+	-- Test case 2 (for UUT2 only)
+        a_tb_1 <= "00101011";
+	-- enable is active low, so 1 means do not enable
+        en_tb_1 <= '1';
         wait for 10 ns;
 	
 	report "Running Test case 2";
-	assert y_tb = "ZZZZZZZZ" report "Test 1: y_tb should equal ZZZZZZZZ" severity error;
+	assert y_tb = "ZZZZZZZZ" report "Test 2: y_tb should equal ZZZZZZZZ" severity error;
+
+	
+	-- Test two tri state buffers at the same time
+
+	-- Test case 3 (For UUT1 and UUT2)
+
+	-- Set UUT1 to enable
+	a_tb_1 <= "11110000";
+        en_tb_1 <= '0';
+        wait for 10 ns;
+
+	-- Set UUT2 to not enabled
+	a_tb_2 <= "00001111";
+        en_tb_2 <= '1';
+        wait for 10 ns;
+
+	-- Only UUT1 output should be visible
+	report "Running Test case 3";
+	assert y_tb = "11110000" report "Test 3: y_tb should equal 11110000" severity error;
+
+	-- Test case 4 (For UUT1 and UUT2)
+
+	-- Set UUT1 to not enabled
+	a_tb_1 <= "11110000";
+        en_tb_1 <= '1';
+        wait for 10 ns;
+
+	-- Set UUT2 to enabled
+	a_tb_2 <= "00001111";
+        en_tb_2 <= '0';
+        wait for 10 ns;
+
+	-- Only UUT2 output should be visible
+	report "Running Test case 4";
+	assert y_tb = "00001111" report "Test 4: y_tb should equal 00001111" severity error;
+
 
         -- Add more test cases here if needed
 
