27059a4599e1495c06948dec4823b59f998c6641
Matthew Howey <matthew.howey@yahoo.co.uk>
2023-06-29 19:57:21 +0100

    remove remainder logic from shr

--- SHR_Component.vhd
+++ SHR_Component.vhd
@@ -5,15 +5,12 @@ use ieee.std_logic_1164.all;
 entity SHR_Component is
     port (
         a : in std_logic_vector(7 downto 0);
-	remain_in: in std_logic;
-        y : out std_logic_vector(7 downto 0);
-        remain_out : out std_logic
+        y : out std_logic_vector(7 downto 0)
     );
 end entity SHR_Component;
 
 architecture Behavioral of SHR_Component is
 begin
-    remain_out <= a(0);
     y(0) <= a(1);
     y(1) <= a(2);
     y(2) <= a(3);
@@ -21,6 +18,6 @@ begin
     y(4) <= a(5);
     y(5) <= a(6);
     y(6) <= a(7);
-    y(7) <= remain_in;
+    y(7) <= '0';
 end architecture Behavioral;
 
