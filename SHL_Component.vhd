fc0117eed7f18981652e2b699da196cd5eee25e4
Matthew Howey <matthew.howey@yahoo.co.uk>
2023-06-29 20:08:06 +0100

    remove carry flag assignments from SHL

--- SHL_Component.vhd
+++ SHL_Component.vhd
@@ -5,15 +5,13 @@ use ieee.std_logic_1164.all;
 entity SHL_Component is
     port (
         a : in std_logic_vector(7 downto 0);
-	cin: in std_logic;
-        y : out std_logic_vector(7 downto 0);
-        cout : out std_logic
+        y : out std_logic_vector(7 downto 0)
     );
 end entity SHL_Component;
 
 architecture Behavioral of SHL_Component is
 begin
-    y(0) <= cin;
+    y(0) <= '0';
     y(1) <= a(0);
     y(2) <= a(1);
     y(3) <= a(2);
@@ -21,5 +19,4 @@ begin
     y(5) <= a(4);
     y(6) <= a(5);
     y(7) <= a(6);
-    cout <= a(7);
 end architecture Behavioral;
--- TestBenchFiles/SHL_Component_TB.vhd
+++ TestBenchFiles/SHL_Component_TB.vhd
@@ -12,26 +12,20 @@ architecture Behavioral of SHL_Component_TB is
     component SHL_Component
         port (
             a       : in  std_logic_vector(7 downto 0);
-	    cin : in std_logic;
-            y       : out std_logic_vector(7 downto 0);
-	    cout  : out std_logic
+            y       : out std_logic_vector(7 downto 0)
         );
     end component SHL_Component;
 
     -- Signal declarations
     signal a_tb    : std_logic_vector(7 downto 0);
-    signal cin_tb    : std_logic;
     signal y_tb    : std_logic_vector(7 downto 0);
-    signal cout_tb    : std_logic;
 
 begin
     -- Instantiate the SHL_Component module
     UUT: SHL_Component
         port map (
             a       => a_tb,
-	    cin  => cin_tb,
-            y       => y_tb,
-	    cout => cout_tb
+            y       => y_tb
         );
 
     -- Stimulus process to apply test vectors
@@ -43,49 +37,38 @@ begin
 
         -- Test case 1
         a_tb <= "00000000";
-	cin_tb <= '0';
         wait for 10 ns;
 
 	report "Running Test case 1";
 	assert y_tb = "00000000" report "y_tb should equal 00000000" severity error;
-	assert cout_tb = '0' report "cout_tb should equal 0" severity error;
-
 
         -- Test case 2
         a_tb <= "11111111";
-	cin_tb <= '1';
         wait for 10 ns;
 
 	report "Running Test case 2";
-	assert y_tb = "11111111" report "y_tb should equal 11111111" severity error;
-	assert cout_tb = '1' report "cout_tb should equal 1" severity error;
+	assert y_tb = "11111110" report "y_tb should equal 11111110" severity error;
 
         -- Test case 3
         a_tb <= "10101010";
-	cin_tb <= '0';
         wait for 10 ns;
 
 	report "Running Test case 3";
-	assert y_tb = "01010100" report "y_tb should equal 01010100" severity error;
-	assert cout_tb = '1' report "cout_tb should equal 1" severity error;
+	assert y_tb = "10101000" report "y_tb should equal 10101000" severity error;
 
 	-- Test case 4
         a_tb <= "01010101";
-	cin_tb <= '1';
         wait for 10 ns;
 
 	report "Running Test case 4";
-	assert y_tb = "10101011" report "y_tb should equal 10101011" severity error;
-	assert cout_tb = '0' report "cout_tb should equal 0" severity error;
+	assert y_tb = "10101010" report "y_tb should equal 10101010" severity error;
 
 	-- Test case 5
         a_tb <= "10110101";
-	cin_tb <= '0';
         wait for 10 ns;
 
 	report "Running Test case 5";
 	assert y_tb = "01101010" report "y_tb should equal 01101010" severity error;
-	assert cout_tb = '1' report "cout_tb should equal 1" severity error;
 
         -- Add more test cases here if needed
 
