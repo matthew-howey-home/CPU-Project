06bf34fc3c94a3498100421fa477565428cd068e
Matthew Howey <matthew.howey@yahoo.co.uk>
2023-07-05 20:32:48 +0100

    refactor OR Component

--- OR_Component.vhd
+++ OR_Component.vhd
@@ -3,13 +3,13 @@ use ieee.std_logic_1164.all;
 
 entity OR_Component is
     port (
-        a       : in  std_logic_vector(7 downto 0);
-        b       : in  std_logic_vector(7 downto 0);
-        y	: out std_logic_vector(7 downto 0)
+        input_1		: in  std_logic_vector(7 downto 0);
+        input_2		: in  std_logic_vector(7 downto 0);
+       	output		: out std_logic_vector(7 downto 0)
     );
 end entity OR_Component;
 
 architecture Behavioral of OR_Component is
 begin
-    y <= a or b;
+    output <= input_1 or input_2;
 end architecture Behavioral;
