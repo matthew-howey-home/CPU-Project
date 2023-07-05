c3aeef25eac9723c3445599e8d89f18df28a0790
Matthew Howey <matthew.howey@yahoo.co.uk>
2023-07-05 20:41:54 +0100

    refactor 1bit Tristate buffer Component

--- One_Bit_Tristate_Buffer.vhd
+++ One_Bit_Tristate_Buffer.vhd
@@ -4,10 +4,10 @@ use IEEE.STD_LOGIC_1164.all;
 
 entity One_Bit_Tristate_Buffer is
     Port ( 
-           a  : in  std_logic;
-	   -- enable is active low
-           en  : in  std_logic;
-           y : out std_logic
+           input	: in  std_logic;
+	   -- enable is active low, so '0' = enabled, '1' = not enabled
+           enable 	: in  std_logic;
+           output	: out std_logic
     );
 end One_Bit_Tristate_Buffer;
 
@@ -15,5 +15,5 @@ architecture Behavioral of One_Bit_Tristate_Buffer is
 
 begin    
     --  input/output active low enabled tri-state buffer
-    y <= a when (en = '0') else 'Z';
+    output <= input when (enable = '0') else 'Z';
 end Behavioral;
--- TestBenchFiles/One_Bit_Tristate_Buffer_Test.vhd
+++ TestBenchFiles/One_Bit_Tristate_Buffer_Test.vhd
@@ -4,42 +4,42 @@ use ieee.std_logic_1164.all;
 use ieee.std_logic_textio.all;
 use std.textio.all;
 
-entity One_Bit_Tristate_Buffer_TB is
-end entity One_Bit_Tristate_Buffer_TB;
+entity One_Bit_Tristate_Buffer_Test is
+end entity One_Bit_Tristate_Buffer_Test;
 
-architecture Behavioral of One_Bit_Tristate_Buffer_TB is
+architecture Behavioral of One_Bit_Tristate_Buffer_Test is
     -- Component declaration for the One_Bit_Tristate_Buffer module
     component One_Bit_Tristate_Buffer
         port (
-            a       : in  std_logic;
-	    en      : in std_logic;
-            y       : out std_logic
+            input       : in  std_logic;
+	    enable      : in std_logic;
+            output       : out std_logic
         );
     end component One_Bit_Tristate_Buffer;
 
     -- Signal declarations
-    signal a_tb_1   : std_logic;
-    signal en_tb_1 : std_logic;
+    signal input_test_buffer1   : std_logic;
+    signal enable_test_buffer1 : std_logic;
     
-    signal a_tb_2   : std_logic;
-    signal en_tb_2 : std_logic;
+    signal input_test_buffer2   : std_logic;
+    signal enable_test_buffer2 : std_logic;
 
-    signal y_tb   : std_logic;
+    signal output_test   : std_logic;
    
 begin
     -- Instantiate the Tristate_Buffer module
     UUT1: One_Bit_Tristate_Buffer
         port map (
-            a       => a_tb_1,
-            en       => en_tb_1,
-            y    => y_tb
+            input	=> input_test_buffer1,
+            enable	=> enable_test_buffer1,
+            output	=> output_test
         );
 
     UUT2: One_Bit_Tristate_Buffer
         port map (
-            a       => a_tb_2,
-            en       => en_tb_2,
-            y    => y_tb
+            input	=> input_test_buffer2,
+            enable	=> enable_test_buffer2,
+            output	=> output_test
         );
 
     -- Stimulus process to apply test vectors
@@ -50,39 +50,39 @@ begin
 
 	-- Set UUT2 to not enabled so we can test UUT1 in isolation
 	-- note enable is active low, so 1 means not enabled
-	en_tb_2 <= '1';
+	enable_test_buffer2 <= '1';
 
 	-- Test two tri state buffers at the same time
 
 	-- Test case 1 (For UUT1 and UUT2)
 
 	-- Set UUT1 to enable
-	a_tb_1 <= '1';
-        en_tb_1 <= '0';
+	input_test_buffer1	<= '1';
+        enable_test_buffer1	<= '0';
 
 	-- Set UUT2 to not enabled
-	a_tb_2 <= '0';
-        en_tb_2 <= '1';
+	input_test_buffer2	<= '0';
+        enable_test_buffer2	<= '1';
         wait for 10 ns;
 
 	-- Only UUT1 output should be visible
 	report "Running Test case 1";
-	assert y_tb = '1' report "Test 1: y_tb should equal 1" severity error;
+	assert output_test = '1'	report "Test 1: output_test should equal 1" severity error;
 
 	-- Test case 2 (For UUT1 and UUT2)
 
 	-- Set UUT1 to not enabled
-	a_tb_1 <= '1';
-        en_tb_1 <= '1';
+	input_test_buffer1	<= '1';
+        enable_test_buffer1	<= '1';
 
 	-- Set UUT2 to enabled
-	a_tb_2 <= '0';
-        en_tb_2 <= '0';
+	input_test_buffer2	<= '0';
+        enable_test_buffer2	<= '0';
         wait for 10 ns;
 
 	-- Only UUT2 output should be visible
 	report "Running Test case 2";
-	assert y_tb = '0' report "Test 2: y_tb should equal 0" severity error;
+	assert output_test = '0'	report "Test 2: output_test should equal 0" severity error;
 
 
         -- Add more test cases here if needed
