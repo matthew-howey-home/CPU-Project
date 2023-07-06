4ed8c0ac381fd7bf482074347f31fea4dd15b54f
Matthew Howey <matthew.howey@yahoo.co.uk>
2023-07-06 19:11:08 +0100

    refactor not component

--- NOT_Component.vhd
+++ NOT_Component.vhd
@@ -4,12 +4,12 @@ use ieee.std_logic_1164.all;
 
 entity NOT_Component is
     port (
-        a       : in  std_logic_vector(7 downto 0);
-        y	: out std_logic_vector(7 downto 0)
+        input	: in  std_logic_vector(7 downto 0);
+        output	: out std_logic_vector(7 downto 0)
     );
 end entity NOT_Component;
 
 architecture Behavioral of NOT_Component is
 begin
-    y <= not a;
+    output <= not input;
 end architecture Behavioral;
--- TestBenchFiles/NOT_Component_Test.vhd
+++ TestBenchFiles/NOT_Component_Test.vhd
@@ -4,28 +4,28 @@ use ieee.std_logic_1164.all;
 use ieee.std_logic_textio.all;
 use std.textio.all;
 
-entity NOT_Component_TB is
-end entity NOT_Component_TB;
+entity NOT_Component_Test is
+end entity NOT_Component_Test;
 
-architecture Behavioral of NOT_Component_TB is
+architecture Behavioral of NOT_Component_Test is
     -- Component declaration for the NOT_Component module
     component NOT_Component
         port (
-            a       : in  std_logic_vector(7 downto 0);
-            y       : out std_logic_vector(7 downto 0)
+            input	: in  std_logic_vector(7 downto 0);
+            output	: out std_logic_vector(7 downto 0)
         );
     end component NOT_Component;
 
     -- Signal declarations
-    signal a_tb    : std_logic_vector(7 downto 0);
-    signal y_tb    : std_logic_vector(7 downto 0);
+    signal input_test    : std_logic_vector(7 downto 0);
+    signal output_test    : std_logic_vector(7 downto 0);
 
 begin
     -- Instantiate the NOT_Component module
     UUT: NOT_Component
         port map (
-            a       => a_tb,
-            y       => y_tb
+            input	=> input_test,
+            output	=> output_test
         );
 
     -- Stimulus process to apply test vectors
@@ -35,25 +35,25 @@ begin
 	report "Running Tests for NOT Component";
 
         -- Test case 1
-        a_tb <= "00000000";
+        input_test <= "00000000";
         wait for 10 ns;
 
 	report "Running Test case 1";
-	assert y_tb = "11111111" report "Test 1: y_tb should equal 11111111" severity error;
+	assert output_test = "11111111"		report "Test 1: output_test should equal 11111111" severity error;
 
         -- Test case 2
-        a_tb <= "11111111";
+        input_test <= "11111111";
         wait for 10 ns;
 
 	report "Running Test case 2";
-	assert y_tb = "00000000" report "Test 2: y_tb should equal 00000000" severity error;
+	assert output_test = "00000000"		report "Test 2: output_test should equal 00000000" severity error;
 
 	-- Test case 3
-        a_tb <= "10110101";
+        input_test <= "10110101";
         wait for 10 ns;
 
 	report "Running Test case 3";
-	assert y_tb = "01001010" report "Test 3: y_tb should equal 01001010" severity error;
+	assert output_test = "01001010"		report "Test 3: output_test should equal 01001010" severity error;
 
         -- Add more test cases here if needed
 
