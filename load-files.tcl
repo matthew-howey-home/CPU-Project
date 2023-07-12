348aa85b2349fe5d9ea08bdd0ac4cf591201f131
Matthew Howey <matthew.howey@yahoo.co.uk>
2023-07-12 20:26:54 +0100

    add compile instructions to tcl script

--- load-files.tcl
+++ load-files.tcl
@@ -2,6 +2,8 @@ project open CPUProject2
 
 set baseFilePath "..\\VHDLProjectFiles\\CPUProject\\MainProject\\"
 
+# -------- Add CPU Components to Project for Editing ---------
+
 proc addFileWithBase {filePath projectFolder} {
     global baseFilePath
     set fullPath [file join $baseFilePath $filePath]
@@ -35,3 +37,32 @@ catch {project addfolder General-Utils MainProject}
 addFileWithBase General-Utils\\Eight_Bit_Tristate_Buffer.vhd General-Utils
 addFileWithBase General-Utils\\One_Bit_Tristate_Buffer.vhd General-Utils
 addFileWithBase General-Utils\\Three_Bit_Decoder.vhd General-Utils
+
+# -------- Compile CPU Components ---------
+
+proc compileWithBase {filePath} {
+    global baseFilePath
+    set fullPath [file join $baseFilePath $filePath]
+    vcom $fullPath
+}
+
+# General Utils
+compileWithBase General-Utils\\Eight_Bit_Tristate_Buffer.vhd
+compileWithBase General-Utils\\One_Bit_Tristate_Buffer.vhd
+compileWithBase General-Utils\\Three_Bit_Decoder.vhd
+
+# ALU Utils
+compileWithBase ALU\\ALU-Utils\\One_Bit_Adder.vhd
+
+# ALU Components
+compileWithBase ALU\\ALU-Components\\ADD_Component.vhd
+compileWithBase ALU\\ALU-Components\\SUB_Component.vhd 
+compileWithBase ALU\\ALU-Components\\NOT_Component.vhd
+compileWithBase ALU\\ALU-Components\\AND_Component.vhd
+compileWithBase ALU\\ALU-Components\\OR_Component.vhd
+compileWithBase ALU\\ALU-Components\\SHL_Component.vhd
+compileWithBase ALU\\ALU-Components\\SHR_Component.vhd
+compileWithBase ALU\\ALU-Components\\XOR_Component.vhd
+
+# ALU
+compileWithBase ALU\\ALU.vhd
