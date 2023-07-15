d5879d9ed21d6e46835b88a9373cb25c8b087053
Matthew Howey <matthew.howey@yahoo.co.uk>
2023-07-15 20:55:15 +0100

    refactor sr latch

--- MainProject/Storage-Components/SR-latch.vhd
+++ MainProject/Storage-Components/SR-latch.vhd
@@ -4,21 +4,21 @@ use ieee.std_logic_1164.all;
 
 entity SR_Latch is
     port (
-        S : in std_logic;         -- Set input (active low)
-        R : in std_logic;         -- Reset input (active low)
-        Q : out std_logic;        -- Output Q
-        QN : out std_logic        -- Output Q complement
+        S		: in std_logic;        -- Set input (active low)
+        R		: in std_logic;        -- Reset input (active low)
+        Q		: out std_logic;       -- Output Q
+        Q_Complement	: out std_logic        -- Output Q complement
     );
 end entity SR_Latch;
 
 architecture Behavioral of SR_Latch is
-    signal internal_q : std_logic;
-    signal internal_q_complement : std_logic;
+    signal internal_q			: std_logic;
+    signal internal_q_complement	: std_logic;
 begin
     -- NAND gates implementation
-    internal_q_complement <= R nand internal_q;
-    internal_q <= S nand internal_q_complement;
+    internal_q_complement	<= R nand internal_q;
+    internal_q			<= S nand internal_q_complement;
 
-    Q <= internal_q;
-    QN <= internal_q_complement;
+    Q			<= internal_q;
+    Q_Complement	<= internal_q_complement;
 end architecture Behavioral;
--- TestBenchFiles/SR_Latch_Test.vhd
+++ TestBenchFiles/SR_Latch_Test.vhd
@@ -3,84 +3,77 @@
 library ieee;
 use ieee.std_logic_1164.all;
 
-entity TB_SR_Latch is
-end entity TB_SR_Latch;
+entity SR_Latch_Test is
+end entity SR_Latch_Test;
 
-architecture behavior of TB_SR_Latch is
+architecture behavior of SR_Latch_Test is
     -- Component declaration for the SR Latch
     component SR_Latch is
         port (
-            S : in std_logic;         -- Set input (active low)
-            R : in std_logic;         -- Reset input (active low)
-            Q : out std_logic;        -- Output Q
-            QN : out std_logic        -- Output Q complement
+            S			: in std_logic;        -- Set input (active low)
+            R			: in std_logic;        -- Reset input (active low)
+            Q			: out std_logic;       -- Output Q
+            Q_Complement	: out std_logic        -- Output Q complement
         );
     end component SR_Latch;
 
     -- Signal declarations
-    signal S_tb : std_logic;
-    signal R_tb : std_logic;
-    signal Q_tb : std_logic;
-    signal QN_tb : std_logic;
+    signal S_Test		: std_logic;
+    signal R_Test		: std_logic;
+    signal Q_Test		: std_logic;
+    signal Q_Complement_Test	: std_logic;
 
 begin
     -- Instantiate the SR Latch
     UUT: SR_Latch port map (
-        S => S_tb,
-        R => R_tb,
-        Q => Q_tb,
-        QN => QN_tb
+        S		=> S_Test,
+        R		=> R_Test,
+        Q		=> Q_Test,
+        Q_Complement	=> Q_Complement_Test
     );
 
     -- Stimulus process
     stim_proc: process
     begin
-        -- ****** 1: Test Setting Latch, so Q output is 1, QN is 0
+	-- Initialise Quiescent state (1 is quiescent as they are active low)
+ 	S_Test	<= '1';
+        R_Test	<= '1';
 
-	-- Quiescent state (1 is quiescent as they are active low)
- 	S_tb <= '1';
-        R_tb <= '1';
         wait for 10 ns;
+        -- ****** 1: Test Setting Latch, so Q output is 1, Q_Complement is 0
+
+	-- Set to 'on' (S = '0' means set as it is active low)
+	S_Test	<= '0';
+        R_Test	<= '1';
+        wait for 10 ns;
+
+	-- Return to quiescent state (1 is quiescent as they are active low)
+	S_Test	<= '1';
+        R_Test	<= '1';
+        wait for 10 ns;
+
+	report "Test Setting Latch";
+        assert Q_Test			= '1'	report "Error: Q_Test should be '1'." severity error;
+        assert Q_Complement_Test	= '0'	report "Error: Q_Complement_Test should be '0'." severity error;
+
+
+	-- ****** 2: Test Resetting Latch, so Q output is 0, Q_Complement is 1.
         
-	-- Set to 'on' (S = '0' means set as it is active low
-	S_tb <= '0';
-        R_tb <= '1';
+	-- Reset to 'off' (R = '0' means Reset as it is active low)
+	S_Test	<= '1';
+        R_Test	<= '0';
         wait for 10 ns;
 
-	-- Quiescent state (1 is quiescent as they are active low)
-	S_tb <= '1';
-        R_tb <= '1';
+	-- Return to quiescent state (1 is quiescent as they are active low)
+	S_Test	<= '1';
+        R_Test	<= '1';
         wait for 10 ns;
 
-	report "Testing Setting Latch";
-        assert Q_tb = '1' report "Error: Q_tb should be '1'." severity error;
-        assert QN_tb = '0' report "Error: QN_tb should be '0'." severity error;
+	report "Test Resetting Latch";
+        assert Q_Test			= '0'	report "Error: Q_Test should be '0'." severity error;
+        assert Q_Complement_Test	= '1'	report "Error: Q_Complement_Test should be '1'." severity error;
 
-
-	-- ****** 12: Test Resetting Latch, so Q output is 0, QN is 1.
-
-	-- Quiescent state (1 is quiescent as they are active low)
- 	S_tb <= '1';
-        R_tb <= '1';
-        wait for 10 ns;
-        
-	-- Set to 'on' (S = '0' means set as it is active low
-	S_tb <= '1';
-        R_tb <= '0';
-        wait for 10 ns;
-
-	-- Quiescent state (1 is quiescent as they are active low)
-	S_tb <= '1';
-        R_tb <= '1';
-        wait for 10 ns;
-
-	report "Testing Resetting Latch";
-        assert Q_tb = '0' report "Error: Q_tb should be '0'." severity error;
-        assert QN_tb = '1' report "Error: QN_tb should be '1'." severity error;
-
-
-
-          -- End simulation
+        -- End simulation
         wait;
     end process stim_proc;
 
