dcfa739da94e5f09005d8695e55abc667f26ef46
Matthew Howey <matthew.howey@yahoo.co.uk>
2023-07-25 19:58:25 +0100

    Refactor content of ALU Interface

--- MainProject/ALU_Interface.vhd
+++ MainProject/ALU_Interface.vhd
@@ -8,106 +8,119 @@ use ieee.std_logic_1164.all;
 
 entity  ALU_Interface is
     port (
-        opcode  				: in std_logic_vector(2 downto 0);
-	input_1					: in std_logic_vector(7 downto 0);
-        input_2					: in std_logic_vector(7 downto 0);
-	carry_in				: in std_logic;
-	negative_in				: in std_logic;
-	temp_input_register_input_enable	: in std_logic;
 	Clock					: in std_logic;
-	clear_carry_flag_control		: in std_logic;
-	clear_negative_flag_control		: in std_logic;
-	clear_zero_flag_control			: in std_logic;
-	operation_enable			: in std_logic;
-	Output_Enable				: in std_logic;
+       
 
-        output					: out std_logic_vector(7 downto 0);
-	carry_flag_output			: out std_logic;
-	negative_flag_output			: out std_logic
+	-- main inputs
+	Opcode  				: in std_logic_vector(2 downto 0);
+	Input_Operand_1				: in std_logic_vector(7 downto 0);
+        Input_Operand_2				: in std_logic_vector(7 downto 0);
+	Input_Carry				: in std_logic;
+	Input_Negative				: in std_logic;
+	
+	-- Enable Controls
+	Enable_Input_For_Temp_Input_Reg		: in std_logic;
+	Enable_Operation			: in std_logic;
+	Enable_Output_Final			: in std_logic;
+	
+	
+	-- Other Control Signals
+
+	Control_Clear_Carry			: in std_logic;
+	Control_Clear_Negative			: in std_logic;
+	Control_Clear_Zero			: in std_logic;
+
+	-- Final Outputs
+	Output_Final				: out std_logic_vector(7 downto 0);
+	Output_From_Carry_Flag			: out std_logic;
+	Output_From_Negative_Flag		: out std_logic
     );
 end entity  ALU_Interface;
 
 architecture Behavioral of ALU_Interface is
-   signal temp_input_register_output		: std_logic_vector(7 downto 0);
+   signal Internal_Temp_Input_Reg_Output	: std_logic_vector(7 downto 0);
 
-   signal carry_flag_input			: std_logic;
-   signal negative_flag_input			: std_logic;
-   signal zero_flag_input   			: std_logic;
+   signal Internal_Input_To_Carry_Flag		: std_logic;
+   signal Internal_Input_To_Negative_Flag	: std_logic;
+   signal Internal_Input_To_Zero_Flag   	: std_logic;
 
-   signal carry_flag_internal_output		: std_logic;
-   signal negative_flag_internal_output		: std_logic;
-   signal zero_flag_output   			: std_logic;
+   signal Internal_Output_From_Carry_Flag	: std_logic;
+   signal Internal_Output_From_Negative_Flag	: std_logic;
+   signal Internal_Output_From_Zero_Flag   	: std_logic;
 
-   signal internal_carry_out			: std_logic;
-   signal internal_negative_out			: std_logic;
-   signal internal_zero_out			: std_logic;
+   signal Internal_Carry_Out_From_Op		: std_logic;
+   signal Internal_Negative_Out_From_Op		: std_logic;
+   -- signal internal_zero_out			: std_logic;
 
-   signal ALU_output				: std_logic_vector(7 downto 0);
-   signal operation_result_register_output	: std_logic_vector(7 downto 0);
+   signal Internal_Output_From_ALU		: std_logic_vector(7 downto 0);
+   signal Internal_Output_From_Result_Register	: std_logic_vector(7 downto 0);
 
    begin
-	temp_input_register	: entity work.Eight_Bit_Register
+	Temp_Input_Reg		: entity work.Eight_Bit_Register
         	port map (
-	    		Data_Input		=> input_1,
-			Input_Enable		=> temp_input_register_input_enable,
+	    		Data_Input		=> Input_Operand_1,
+			Input_Enable		=> Enable_Input_For_Temp_Input_Reg,
 			Clock			=> Clock,
             		Output_Enable		=> '1',
 
-			Output			=> temp_input_register_output
+			Output			=> Internal_Temp_Input_Reg_Output
         	);
 	
-	-- carry flag is set by the result of an operation (internal_carry_out)
-	-- but if the clear_carry_flag_control bit is set, this overrides it and sets the carry flag to zero. 
-	carry_flag_input <= (not clear_carry_flag_control) and internal_carry_out;
+	-- carry flag is set by the result of an operation (Internal_Carry_Out_From_Op)
+	-- but if the Control_Clear_Carry bit is set, this overrides it and sets the carry flag to zero. 
+	Internal_Input_To_Carry_Flag <= (not Control_Clear_Carry) and Internal_Carry_Out_From_Op;
 
-	carry_flag		: entity work.Edge_Triggered_Flip_Flop
+	Carry_Flag		: entity work.Edge_Triggered_Flip_Flop
 		port map (
-			Data_Input		=> carry_flag_input,
+			Data_Input		=> Internal_Input_To_Carry_Flag,
 			Input_Enable		=> '1',
 			Clock			=> Clock,
-			output			=> carry_flag_internal_output
+			output			=> Internal_Output_From_Carry_Flag
 		);
 
 	-- negative flag is set by the result of an operation (internal_negative_out)
 	-- but if the clear_negative_flag_control bit is set, this overrides it and sets the negative flag to zero. 
-	negative_flag_input <= not clear_negative_flag_control and internal_negative_out;
+	Internal_Input_To_Negative_Flag <= not Control_Clear_Negative and Internal_Negative_Out_From_Op;
 
 	negative_flag		: entity work.Edge_Triggered_Flip_Flop
 		port map (
-			Data_Input		=> negative_flag_input,
+			Data_Input		=> Internal_Input_To_Negative_Flag,
 			Input_Enable		=> '1',
 			Clock			=> Clock,
-			output			=> negative_flag_internal_output
+			output			=> Internal_Output_From_Negative_Flag
 		);
+	
 	
 	ALU			: entity work.ALU
 		port map (
-	    		opcode			=> opcode,
-			input_1			=> temp_input_register_output,
-			input_2			=> input_2,
+	    		opcode			=> Opcode,
+			-- first input is read from temporary input register
+			-- second input is read directly from the bus 
+			input_1			=> Internal_Temp_Input_Reg_Output,
+			input_2			=> Input_Operand_2,
 			-- carry in is the result of the previous carry out
-			carry_in		=> carry_flag_internal_output,
+			carry_in		=> Internal_Output_From_Carry_Flag,
 			-- negative in is the result of the previous negative out
-			negative_in		=> negative_flag_internal_output,
+			negative_in		=> Internal_Output_From_Negative_Flag,
 	
-			output			=> ALU_output,
-			carry_out		=> internal_carry_out,
-			negative_out		=> internal_negative_out
+			output			=> Internal_Output_From_ALU,
+			carry_out		=> Internal_Carry_Out_From_Op,
+			negative_out		=> Internal_Negative_Out_From_Op
         	);
 
-	-- loads result of operation, ready for output
-	operation_result_register	: entity work.Eight_Bit_Register
+	-- Load result of operation, ready for Final Output
+	Operation_Result_Register	: entity work.Eight_Bit_Register
         	port map (
-	    		Data_Input		=> ALU_output,
-			Input_Enable		=> operation_enable,
+	    		Data_Input		=> Internal_Output_From_ALU,
+			Input_Enable		=> Enable_Operation,
 			Clock			=> Clock,
-            		Output_Enable		=> Output_Enable,
+            		Output_Enable		=> Enable_Output_Final,
 
-			Output			=>  operation_result_register_output
+			Output			=> Internal_Output_From_Result_Register
         	);
 	
-	carry_flag_output			<= carry_flag_internal_output;
-	negative_flag_output			<= negative_flag_internal_output;
-	output					<= operation_result_register_output;
+	Output_From_Carry_Flag			<= Internal_Output_From_Carry_Flag;
+	Output_From_Negative_Flag		<= Internal_Output_From_Negative_Flag;
+	Output_Final				<= Internal_Output_From_Result_Register;
 
 end architecture Behavioral;
