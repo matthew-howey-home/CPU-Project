b325fbef26721e30bfc40ac486e3b283eece0cf5
Matthew Howey <matthew.howey@yahoo.co.uk>
2023-11-05 19:48:20 +0000

    add reset function for PC

--- MainProject/CPU.vhd
+++ MainProject/CPU.vhd
@@ -12,23 +12,49 @@ architecture Behavioral of CPU is
 	signal FSM_Register_Initial_State	: std_logic_vector(7 downto 0);
 	signal FSM_Register_In			: std_logic_vector(7 downto 0);
 
+	signal PC_Low_Initial_State		: std_logic_vector(7 downto 0);
+	signal PC_Low_In			: std_logic_vector(7 downto 0);
+	signal PC_High_Initial_State		: std_logic_vector(7 downto 0);
+	signal PC_High_In			: std_logic_vector(7 downto 0);
+
 	signal Decoder_FSM_In			: std_logic_vector(7 downto 0);
 	signal Decoder_FSM_Out			: std_logic_vector(7 downto 0);
-	signal Instruction	: std_logic_vector(7 downto 0);
-	signal Control_Bus	: std_logic_vector(13 downto 0);
 
+	signal Instruction			: std_logic_vector(7 downto 0);
+	signal Control_Bus			: std_logic_vector(13 downto 0);
+	signal Data_Bus				: std_logic_vector(7 downto 0);
 	
 begin
-	FSM_Register_Initial_State <= "00000001";
+	FSM_Register_Initial_State 	<= "00000001";
+	PC_Low_Initial_State 		<= "00000000";
+	PC_High_Initial_State 		<= "00000000";
 
-	-- RESET MUX, connect FSM_Initial_State to FSM input if Reset is asserted
-	Reset_Mux: entity work.Two_to_One_Byte_Mux
+	-- RESET FSM MUX, connect FSM_Initial_State to FSM input if Reset is asserted
+	Reset_FSM_Mux: entity work.Two_to_One_Byte_Mux
 		port map (
 	    		input_1 	=> Decoder_FSM_Out, -- i.e. next FSM state as output from decoder
             		Input_2 	=> FSM_Register_Initial_State,
             		selector	=> Reset,
 	
             		Output 		=> FSM_Register_In
+        	);
+
+	Reset_PC_Low_Mux: entity work.Two_to_One_Byte_Mux
+		port map (
+	    		input_1 	=> Data_Bus, -- default input to PC Low
+            		Input_2 	=> PC_Low_Initial_State,
+            		selector	=> Reset,
+	
+            		Output 		=> PC_Low_In
+        	);
+
+	Reset_PC_High_Mux: entity work.Two_to_One_Byte_Mux
+		port map (
+	    		input_1 	=> Data_Bus, -- default input to PC High
+            		Input_2 	=> PC_High_Initial_State,
+            		selector	=> Reset,
+	
+            		Output 		=> PC_High_In
         	);
 
  
@@ -64,6 +90,26 @@ begin
 			A_Reg_Input_Enable			=> Control_Bus(11), 	
 			X_Reg_Input_Enable			=> Control_Bus(12), 	
 			Y_Reg_Input_Enable			=> Control_Bus(13) 	
+        	);
+	
+	PC_Low: entity work.Eight_Bit_Register
+		port map (
+	    		Data_Input 	=> PC_Low_In,
+            		Input_Enable 	=> '1',
+            		Clock 		=> Clock,
+			Output_Enable 	=> Control_Bus(2),
+
+            		Output 		=> Data_Bus
+        	);
+
+	PC_High: entity work.Eight_Bit_Register
+		port map (
+	    		Data_Input 	=> PC_High_In,
+            		Input_Enable 	=> '1',
+            		Clock 		=> Clock,
+			Output_Enable 	=> Control_Bus(3),
+
+            		Output 		=> Data_Bus
         	);
 	
 end architecture Behavioral;
